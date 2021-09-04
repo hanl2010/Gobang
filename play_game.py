8f7b84ae851e42ba18562113a52be32dfbd1bc28
hanl2010 <hanl2010@foxmail.com>
2021-09-05 01:56:17 +0800

    update human_play

--- play_game.py
+++ play_game.py
@@ -1,12 +1,13 @@
 from board import Board
 from mcts import MCTS
 from mcts_with_policy_fn import MCTS_WITH_POLICY
-# import pygame
+import pygame
 import random
 from evaluate_method import Evaluate
 from neural_network import PolicyNet
 import numpy as np
 import sys
+import time
 
 
 class Game:
@@ -22,26 +23,32 @@ class Game:
             # pygame.display.set_caption("五子棋")
             # self.screen = pygame.display.set_mode(size=self.screen_size)
             # self.board.draw_board(self.screen, self.screen_size)
+            # pygame.event.set_allowed((pygame.MOUSEBUTTONDOWN, pygame.QUIT))
             self.board.print_board()
 
-    # def human_play(self):
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-    #             x, y = event.pos
-    #             action = (round((y-self.board.h_grid_size)/self.board.h_grid_size),
-    #                       round((x-self.board.w_grid_size)/self.board.w_grid_size))
-    #             self.board.move(action)
-    #         elif event.type == pygame.QUIT:
-    #             pygame.quit()
+    def human_play(self):
+        # for event in pygame.event.get():
+        while True:
+            event = pygame.event.wait()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = event.pos
+                action = (round((y-self.board.h_grid_size)/self.board.h_grid_size),
+                          round((x-self.board.w_grid_size)/self.board.w_grid_size))
+                self.board.move(action)
+                break
+            elif event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
     def random_play(self):
         action = random.choice(self.board.get_valid_move_pos())
         self.board.move(action)
 
     def mcts_play(self):
-        mcts = MCTS(c_param=5, n_rollout=1000)
-        mcts.simulation_parallel(self.board)
+        mcts = MCTS(c_param=5, n_rollout=2000)
+        mcts.simulation(self.board)
         action = mcts.get_move()
+        # mcts.print_first_level_node()
         print("pure mcts action", action)
         self.board.move(action)
 
@@ -52,9 +59,9 @@ class Game:
         # action_index = np.argmax(act_probs)
         # self.board.move(actions[action_index])
         ## 2. 使用神经网络 和 MCST 模拟若干次，通过MCST得到action probability
-        mcts = MCTS_WITH_POLICY(policy_func=self.policy_network.get_policy_value, c_param=5, n_rollout=500)
-        # actions_index, act_probs = mcts.simulation(self.board)
-        actions_index, act_probs = mcts.simulation_parallel(self.board)
+        mcts = MCTS_WITH_POLICY(policy_func=self.policy_network.get_policy_value, c_param=1, n_rollout=500)
+        actions_index, act_probs = mcts.simulation(self.board)
+        # actions_index, act_probs = mcts.simulation_parallel(self.board) # 并行方法有问题
         self.policy_network.collect_temp_data(board=self.board, actions_index=actions_index, act_probs=act_probs)
         action = mcts.get_move()
         print("policy mcts action", action)
@@ -79,6 +86,8 @@ class Game:
                     white_strategy()
                 if self.board_show:
                     self.board.print_board()
+                print("evaluate times:{}, black wins:{}, white wins:{}, draws:{}".format(i, black_wins, white_wins,
+                                                                                         draws))
             result = self.board.get_game_result()
             self.board.reset()
             if result==self.board.RESULT_BLACK_WIN:
@@ -91,7 +100,7 @@ class Game:
         return (black_wins+0.5*draws)/self.evaluate_times
 
 
-    def play_game(self, black_strategy, white_strategy, i, results):
+    def play_game(self, black_strategy, white_strategy, i, results, use_network=False):
 
         while self.board.get_game_result() == self.board.RESULT_NOT_OVER:
             print("######## black wins: {}/{}, white_wins: {}/{}, draws: {} #########".format(
@@ -115,28 +124,31 @@ class Game:
                 self.board.print_board()
 
         result = self.board.get_game_result()
-        self.policy_network.calc_and_collect_final_data(result)
-        ## train network
-        self.policy_network.train()
+        if use_network:
+            self.policy_network.calc_and_collect_final_data(result)
+            ## train network
+            self.policy_network.train()
+        # 等待X秒之后，重置棋盘
+        time.sleep(2)
         ## reset board
         self.board.reset()
-        if self.board_show:
-            pass
-            # self.board.draw_board(self.screen, self.screen_size)
+        # if self.board_show:
+        #     self.board.draw_board(self.screen, self.screen_size)
         return result
 
 
 if __name__ == '__main__':
-    game = Game(board_size=11, n_dots=5, board_show=True)
+    game = Game(board_size=7, n_dots=4, board_show=True)
     results = []
     i = 0
     best_win_ratio = 0
+    use_network = False
     while True:
         i += 1
-        result = game.play_game(game.network_play, game.network_play, i, results)
+        result = game.play_game(game.mcts_play, game.mcts_play, i, results, use_network=use_network)
         results.append(result)
 
-        if i%50 == 0:
+        if i%50 == 0 and use_network==True:
             game.policy_network.save_model("current_policy_multiprocess.model")
             win_ratio = game.evaluate(game.network_play, game.mcts_play)
             if win_ratio > best_win_ratio:
